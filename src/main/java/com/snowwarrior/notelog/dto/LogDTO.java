b6acf85b2965199649c63a7ecb11ddca2fc3b460
fxd <mygoddess007700@gmail.com>
2022-09-08 23:09:01 +0800

    feat: 完善LogDTO

--- src/main/java/com/snowwarrior/notelog/dto/LogDTO.java
+++ src/main/java/com/snowwarrior/notelog/dto/LogDTO.java
@@ -1,4 +1,22 @@
 package com.snowwarrior.notelog.dto;
 
+import javax.xml.crypto.Data;
+
 public class LogDTO {
+    private Long id;
+
+    private String location;
+    private Data createAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Data getCreateAt() {
+        return createAt;
+    }
 }
